d970b72e269656a57e0571f3de57daef965a624b
kaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-07-28 13:02:49 +0300

    Added comic book and artist models

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -12,6 +12,7 @@ namespace comic_book_gallery.Controllers
         {
             ViewBag.SeriesTitle = "The Amazing Spider-Man";
             ViewBag.IssueNumber = 700;
+            //use @Html.Raw to accept and display html tags
             ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
             ViewBag.Artists = new string[]
             {
